890905d16dc90a69700a515aa2e7732d1170d7ab
DanielsJJ <d.jaunarajsjanvaris@gmail.com>
2025-01-21 13:49:55 +0200

    Update Login.php
    Uzlaboju kodu

--- Login.php
+++ Login.php
@@ -1,33 +1,29 @@
 <!DOCTYPE html>
-<html lang="lv">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <link rel="stylesheet" href="assets/style.css">
-    <script src="assets/script.js" defer></script>
-</head>
-<body>
-<main class="login-container">
-    <div class="login-box">
-        <h1>Pieteikšanās 5.vsk Datubāzē</h1>
-        <form method="post" action="Inventars.php">
-
-            <div class="input-container">
-                <i class="fas fa-user icon"></i>   
-            <input type="text" id="lietotajs" name="lietotajs" class="input-field" placeholder="Ievadi lietotājvārdu..." required >
-            </div>
-            
-            <div class="input-container">
-                <i class="fas fa-user icon"></i> 
-            <input type="password" id="parole" name="parole" class="input-field" maxlength="255" placeholder="Ievadi paroli..." required>
-            </div>
-            
-            <button type="submit" name="ienakt" class="login-button">Ienākt</button>
-        </form>
-    </div>
-</main>
-</body>
+<html>
+	<head>
+		<meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Login</title>
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+        <link rel="stylesheet" href="assets/style.css">
+        <script src="assets/script.js" defer></script>
+	</head>
+	<body>
+		<div class="login">
+            <h1>5.vsk datubāze</h1>
+			<h2>Login</h2>           
+			<form action="Inventars.php" method="post">
+				<label for="username">
+					<i class="fas fa-user"></i>
+				</label>
+				<input type="text" name="username" placeholder="Lietotājvārds" id="username" required>
+				<label for="password">
+					<i class="fas fa-lock"></i>
+				</label>
+				<input type="password" name="password" placeholder="Parole" id="password" required>
+				<input type="submit" value="Login">
+			</form>
+		</div>
+	</body>
 </html>
 
