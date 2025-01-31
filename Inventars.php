73981e02b8dcbe39b63ae71dc5132aea0338594b
DanielsJJ <d.jaunarajsjanvaris@gmail.com>
2025-01-31 10:24:45 +0200

    Revert "Merge branch 'master' of https://github.com/DJaJa0/PraksesProjekts"
    This reverts commit 92b33a8aa37f21356f9cb3983b5d37c171f3d496, reversing
    changes made to 6d040899c43cb5298839c0337dd32a06a5809653.

--- Inventars.php
+++ Inventars.php
@@ -1,3 +1,13 @@
+<?php
+
+session_start();
+
+if (!isset($_SESSION['loggedin'])) {
+	header('Location: Login.php');
+	exit;
+}
+?>
+
 <!DOCTYPE html>
 <html lang="lv">
 <head>
@@ -5,9 +15,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>DatuBāze</title>
     <link rel="stylesheet" href="assets/style2.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer">
     <script src="assets/script.js" defer></script>
 </head>
-<body>
+
+<body> 
+    <div class="loggedin">
+		<nav class="navtop">
+			<div>
+				<h1>
+                    <img src="assets/photos/icon.png" alt="Skolas logo" class="logo">
+                    Liepājas Draudzīgā aicinājuma vidusskola
+                </h1>
+				<a href="profile.php"><i class="fas fa-user-circle"></i>Profils</a>
+				<a href="logout.php"><i class="fas fa-sign-out-alt"></i>Iziet</a>
+			</div>
+		</nav>
+		<div class="content">
+			<h2>Skolas inventāra datubāze</h2>
+			<p>Sveicināts atpakaļ, <?=htmlspecialchars($_SESSION['name'], ENT_QUOTES)?>!</p>
+		</div>
+    </div>
+        
     <form>
         <label for="inventars">Izvēlaties vienu:</label>
         <select name="inventars" id="inventars">
@@ -44,7 +73,7 @@
 </div>
 
 
-
+<img src="assets/photos/skolasbilde22.jpg" alt="Fona attēls" class="background-img">
 
     
 </body>
