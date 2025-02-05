e30e42d28328ef494e8a6b65a417024c16dcff2e
DanielsJJ <d.jaunarajsjanvaris@gmail.com>
2025-02-05 15:00:42 +0200

    Create dati.php

---
+++ pagFaili/dati.php
@@ -0,0 +1,47 @@
+@ -0,0 +1,46 @@
+<?php
+header('Content-Type: application/json');
+
+$servername = "sql7.freesqldatabase.com";
+$username = "sql7761322";
+$password = "taU4zHrvby";
+$dbname = "sql7761322";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die(json_encode(["error" => "Savienojuma kļūda: " . $conn->connect_error]));
+}
+
+
+if (isset($_GET['tips'])) {
+    $tips = $_GET['tips'];
+
+    $lauki = [
+        "dators" => ["Datora_nosaukums", "Datora_nr"],
+        "monitors" => ["Monitora_nosaukums", "Monitora_nr"],
+        "projektors" => ["Projektora_nosaukums", "Projektora_nr"]
+    ];
+
+    if (isset($lauki[$tips])) {
+        $nosaukums = $lauki[$tips][0];
+        $numurs = $lauki[$tips][1];
+
+        $sql = "SELECT Kab AS kabinets, $nosaukums AS nosaukums, $numurs AS numurs FROM inventarizacija_2024";
+        $result = $conn->query($sql);
+
+        $dati = [];
+        while ($row = $result->fetch_assoc()) {
+            $dati[] = $row;
+        }
+
+        echo json_encode($dati);
+    } else {
+        echo json_encode(["error" => "Nepareizs tips!"]);
+    }
+} else {
+    echo json_encode(["error" => "Trūkst tips parametra!"]);
+}
+
+$conn->close();
+?>
