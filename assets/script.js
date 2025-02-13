dbd6f13e0c24853f01f3357523efddd5b859ad70
DanielsJJ <d.jaunarajsjanvaris@gmail.com>
2025-02-13 18:09:01 +0200

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -190,3 +190,23 @@ function hidePassword() {
     location.reload(); 
 }
 
+//Paroles parādīšana Login.php
+
+document.addEventListener("DOMContentLoaded", function (){
+    const passwordInput = document.getElementById("password");
+    const togglePassword = document.getElementById("togglePassword");
+
+    togglePassword.addEventListener("click", function (){
+        if(passwordInput.type === "password"){
+            passwordInput.type = "text";
+            togglePassword.innerHTML = '<i class="fas fa-eye-slash"></i>';
+        }else{
+            passwordInput.type = "password";
+            togglePassword.innerHTML = '<i class="fas fa-eye"></i>'; 
+        }
+    });
+});
+
+
+
+
