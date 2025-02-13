064a4328aa0f9f02749367e7f27700e50198b6a4
DanielsJJ <d.jaunarajsjanvaris@gmail.com>
2025-02-13 18:08:52 +0200

    Update profile.php

--- phplogin/profile.php
+++ phplogin/profile.php
@@ -39,7 +39,7 @@ $stmt->close();
 		<nav class="navtop">
 			<div>
 				<h1>
-                    <img src="../assets/photos/icon.png" alt="Skolas logo" class="logo">
+					<img src="../assets/photos/icon.png" alt="Skolas logo" class="logo" onclick="window.location.href='../inventars.php';">
                     Liepājas Draudzīgā aicinājuma vidusskola
                 </h1>
 				<a href="profile.php"><i class="fas fa-user-circle"></i>Profils</a>
