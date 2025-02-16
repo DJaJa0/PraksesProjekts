0315266855497d78096f2a30fa07126bd3fa882a
DanielsJJ <d.jaunarajsjanvaris@gmail.com>
2025-02-16 15:44:01 +0200

    Update saglabat.php

--- pagFaili/saglabat.php
+++ pagFaili/saglabat.php
@@ -21,50 +21,80 @@ if ($conn->connect_error) {
 }
 
 // Saņem JSON un pārveido par PHP masīvu
+// Pieņemsim, ka tiek nosūtīts "tips" un atjaunināts ieraksts atbilstoši tam
 $input = json_decode(file_get_contents("php://input"), true);
 
 // Ja nav saņemti dati vai formāts nederīgs
 if (!$input) {
-    error_log("Nepareizs JSON formāts vai dati nav saņemti!"); // Kļūdas reģistrēšana
+    error_log("Nepareizs JSON formāts vai dati nav saņemti!"); 
     echo json_encode(["error" => "Nepareizs JSON formāts vai dati nav saņemti!"]);
     exit;
 }
 
 // Pārbauda, vai ir nepieciešamie lauki
-if (!isset($input["nosaukums"], $input["numurs"], $input["kabinets"])) {
+if (!isset($input["nosaukums"], $input["numurs"], $input["kabinets"], $input["tips"])) {
     error_log("Nepilnīgi dati!"); // Kļūdas reģistrēšana
     echo json_encode(["error" => "Nepilnīgi dati!"]);
     exit;
 }
 
-// SQL drošībai
 $nosaukums = $conn->real_escape_string($input["nosaukums"]);
 $numurs = $conn->real_escape_string($input["numurs"]);
 $kabinets = $conn->real_escape_string($input["kabinets"]);
+$tipaVeids = $input["tips"];  // Ierīces tips
 
-// Ja ir ID, tad tiek labots, citādi pievienots jauns ieraksts
+// SQL pieprasījums atbilstoši izvēlētajai ierīces kategorijai
 if (!empty($input["id"])) {
     $id = intval($input["id"]);
-    $sql = "UPDATE inventarizacija_2024 
-            SET Datora_nosaukums='$nosaukums', Datora_nr='$numurs', Kab='$kabinets' 
-            WHERE id=$id";
-} else {
-    $sql = "INSERT INTO inventarizacija_2024 (Datora_nosaukums, Datora_nr, Kab) 
-            VALUES ('$nosaukums', '$numurs', '$kabinets')";
-}
-
-// Izpilda SQL
-if ($conn->query($sql) === TRUE) {
-    if (!empty($input["id"])) {
-        echo json_encode(["message" => "Ieraksts veiksmīgi atjaunināts", "id" => $input["id"]]);
-    } else {
-        echo json_encode(["message" => "Jauns ieraksts veiksmīgi pievienots", "id" => $conn->insert_id]);
+    switch ($tipaVeids) {
+        case 'dators':
+            $sql = "UPDATE inventarizacija_2024 SET Datora_nosaukums='$nosaukums', Datora_nr='$numurs', Kab='$kabinets' WHERE id=$id";
+            break;
+        case 'monitors':
+            $sql = "UPDATE inventarizacija_2024 SET Monitora_nosaukums='$nosaukums', Monitora_nr='$numurs', Kab='$kabinets' WHERE id=$id";
+            break;
+        case 'projektori':
+            $sql = "UPDATE inventarizacija_2024 SET Projektora_nosaukums='$nosaukums', Projektora_nr='$numurs', Kab='$kabinets' WHERE id=$id";
+            break;
+        case 'tumbas':
+            $sql = "UPDATE inventarizacija_2024 SET Tumbas_nosaukums='$nosaukums', Tumbas_nr='$numurs', Kab='$kabinets' WHERE id=$id";
+            break;
+        case 'printeri':
+            $sql = "UPDATE inventarizacija_2024 SET Printera_nosaukums='$nosaukums', Printera_nr='$numurs', Kab='$kabinets' WHERE id=$id";
+            break;
+        case 'dok':
+            $sql = "UPDATE inventarizacija_2024 SET Dok_kam_nosaukums='$nosaukums', Dok_kam_nr='$numurs', Kab='$kabinets' WHERE id=$id";
+            break;
     }
 } else {
-    error_log("Kļūda saglabājot: " . $conn->error); // Kļūdas reģistrēšana
+    switch ($tipaVeids) {
+        case 'dators':
+            $sql = "INSERT INTO inventarizacija_2024 (Datora_nosaukums, Datora_nr, Kab) VALUES ('$nosaukums', '$numurs', '$kabinets')";
+            break;
+        case 'monitors':
+            $sql = "INSERT INTO inventarizacija_2024 (Monitora_nosaukums, Monitora_nr, Kab) VALUES ('$nosaukums', '$numurs', '$kabinets')";
+            break;
+        case 'projektori':
+            $sql = "INSERT INTO inventarizacija_2024 (Projektora_nosaukums, Projektora_nr, Kab) VALUES ('$nosaukums', '$numurs', '$kabinets')";
+            break;
+        case 'tumbas':
+            $sql = "INSERT INTO inventarizacija_2024 (Tumbas_nosaukums, Tumbas_nr, Kab) VALUES ('$nosaukums', '$numurs', '$kabinets')";
+            break;
+        case 'printeri':
+            $sql = "INSERT INTO inventarizacija_2024 (Printera_nosaukums, Printera_nr, Kab) VALUES ('$nosaukums', '$numurs', '$kabinets')";
+            break;
+        case 'dok':
+            $sql = "INSERT INTO inventarizacija_2024 (Dok_kam_nosaukums, Dok_kam_nr, Kab) VALUES ('$nosaukums', '$numurs', '$kabinets')";
+            break;
+    }
+}
+
+if ($conn->query($sql) === TRUE) {
+    echo json_encode(["message" => "Ieraksts veiksmīgi atjaunināts", "id" => $conn->insert_id]);
+} else {
+    error_log("Kļūda saglabājot: " . $conn->error); 
     echo json_encode(["error" => "Kļūda saglabājot: " . $conn->error]);
 }
 
-// Aizver savienojumu
 $conn->close();
 ?>
