09f083fc7cd2bdfb6907dc28828f4920e10bea69
DanielsJJ <d.jaunarajsjanvaris@gmail.com>
2025-02-17 00:34:34 +0200

    Update kontakti.php

--- kontakti.php
+++ kontakti.php
@@ -12,9 +12,9 @@ if (!isset($_SESSION['loggedin'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Par mums</title>
+    <title>DatuBāze</title>
     <link rel="stylesheet" href="assets/style2.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer">
     <script src="assets/script.js" defer></script>
 </head>
 
@@ -23,20 +23,23 @@ if (!isset($_SESSION['loggedin'])) {
 <div class="content2">
 
     <div class="loggedin">
-        <nav class="navtop">
-            <div>
-                <h1>
-                    <img src="assets/photos/icon.png" alt="Skolas logo" class="logo" onclick="window.location.href='inventars.php';">
+		<nav class="navtop">
+			<div>
+				<h1>
+                <img src="assets/photos/icon.png" alt="Skolas logo" class="logo" onclick="window.location.href='index.php';">
                     Liepājas Draudzīgā aicinājuma vidusskola
                 </h1>
-                <a href="phplogin/profile.php"><i class="fas fa-user-circle"></i> Profils</a>
-                <a href="phplogin/logout.php"><i class="fas fa-sign-out-alt"></i> Iziet</a>
-            </div>
-        </nav>
-        <div class="content">
-            <h2>Kontakti</h2>
-
-            <section class="contacts-container">
+				<a href="phplogin/profile.php"><i class="fas fa-user-circle"></i>Profils</a>
+				<a href="phplogin/logout.php"><i class="fas fa-sign-out-alt"></i>Iziet</a>
+			</div>
+		</nav>
+		<div class="content">
+			<h2>Kontakti</h2>
+		
+		</div>
+    </div>
+        
+    <section class="contacts-container">
                 <div class="contacts-header">
                     <h2><i class="fas fa-phone-alt"></i> Kontakti</h2>
                     <p><strong>Liepājas Draudzīgā aicinājuma vidusskola</strong></p>
@@ -62,32 +65,32 @@ if (!isset($_SESSION['loggedin'])) {
                     </ul>
             </div>
         </section>
-
 </div>
 
 <footer>
-    <div class="footerContainer">
-        <div class="socialIcons">
-            <a href="https://www.facebook.com/5vsk.liepaja.edu.lv/"><i class="fa-brands fa-facebook"></i></a>
-            <a href="https://www.instagram.com/draudzigavsk/"><i class="fa-brands fa-instagram"></i></a>
-            <a href="https://www.youtube.com/@dalp5.vsk.30/videos"><i class="fa-brands fa-youtube"></i></a>
-        </div>
-        <div class="footerNav">
-            <ul>
-                <li><a href="inventars.php">Mājas</a></li>
-                <li><a href="parmums.php">Par mums</a></li>
-                <li><a href="kontakti.php">Kontakti</a></li>
-            </ul>
-        </div>
+<div class="footerContainer">
+    <div class="socialIcons">
+        <a href="https://www.facebook.com/5vsk.liepaja.edu.lv/events/?key=events&ref=page_internal&locale=lv"><i class="fa-brands fa-facebook"></i></a>
+        <a href="https://www.instagram.com/draudzigavsk/"><i class="fa-brands fa-instagram"></i></a>
+        <a href="https://www.youtube.com/@dalp5.vsk.30/videos"><i class="fa-brands fa-youtube"></i></a>
     </div>
+    <div class="footerNav">
+        <ul>
+            <li><a href="index.php">Mājas</a></li>
+            <li><a href="parmums.php">Par mums</a></li>
+            <li><a href="kontakti.php">Kontakti</a></li>
+        </ul>
+    </div>
+    
+</div>
     <div class="footerBottom">
-        <p>&copy;2025 Liepājas Draudzīgā aicinājuma vidusskola</p>
+        <p>&copy;2025; Liepājas Draudzīgā aicinājuma vidusskola</p>
     </div>
 </footer>
 
-<button onclick="scrollToTop()" id="scrollTopBtn" title="Uz augšu">
+    <button onclick="scrollToTop()" id="scrollTopBtn" title="Uz augšu">
     <i class="fas fa-chevron-up"></i>
-</button>
+    </button>
 
 </body>
 </html>
