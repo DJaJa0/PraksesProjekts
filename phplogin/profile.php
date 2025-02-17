1c16253fe716117f753e48cb0957163e13180359
DanielsJJ <d.jaunarajsjanvaris@gmail.com>
2025-02-17 23:02:55 +0200

    Update profile.php

--- phplogin/profile.php
+++ phplogin/profile.php
@@ -28,10 +28,11 @@ $stmt->close();
 <html>
 	<head>
 		<meta charset="utf-8">
-		<title>Profile Page</title>
+		<title>Profils</title>
 		<link href="../assets/style2.css" rel="stylesheet" type="text/css">
 		<script src="../assets/script.js" defer></script>
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer">
+		<link rel="icon" type="image/png" href="../assets/photos/icon.png">
 
 
 	</head>
